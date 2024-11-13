a0bab634bb0e538cb30d5d1561a6f8efa0e6ca8f
naveen prasanth <naveenprasanth2@gmail.com>
2024-11-13 18:57:47 +0530

    initial push

---
+++ src/main/java/com/dailycodebuffer/springbootstratergy/SpringBootStratergyApplication.java
@@ -0,0 +1,13 @@
+package com.dailycodebuffer.springbootstratergy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStratergyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStratergyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dailycodebuffer/springbootstratergy/SpringBootStratergyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailycodebuffer.springbootstratergy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStratergyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
